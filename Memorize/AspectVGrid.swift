7e5d9d9df2547c08cdb3bddfe198f89f0a51e813
Cephei.DZ <Buster2491@gmail.com>
2023-12-27 18:49:31 +0800

    Add AspectVGrid

---
+++ Memorize/AspectVGrid.swift
@@ -0,0 +1,18 @@
+//
+//  AspectVGrid.swift
+//  Memorize
+//
+//  Created by Vic.Z Ding on 2023/12/27.
+//
+
+import SwiftUI
+
+struct AspectVGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AspectVGrid()
+}
